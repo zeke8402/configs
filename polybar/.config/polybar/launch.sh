5f852347c46e57bff1a8ee2a0ec8ba5ff37bbbd7
Zeke <jkezekiel@gmail.com>
2024-12-22 01:45:01 -0600

    integrating pywal into the configs

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -7,7 +7,7 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar -c ~/.config/polybar/config-i3-new top &
-polybar -c ~/.config/polybar/config-i3-new bottom &
+polybar -c ~/.config/polybar/config-i3-pywal top &
+polybar -c ~/.config/polybar/config-i3-pywal bottom &
 
 echo "Bars launched..."
